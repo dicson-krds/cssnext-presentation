5e85e8a9c1ab52b36c19106bd098cd7735025954
dicson-ui <smartdicson@gmail.com>
2017-03-31 17:10:45 +0530

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,17 +10,12 @@ module.exports = function(grunt) {
       },
       postcss: {
          options: {
-            map: {
-               inline: false, // save all sourcemaps as separate files...
-               annotation: 'dist/css/maps/' // ...to the specified directory
-            },
-            diff: 'custom/file.css.patch',
             processors: [
-               require("postcss-cssnext")(),
                require("postcss-import")(),
                require("postcss-url")(),
+               require("postcss-cssnext")(),
                require("postcss-browser-reporter")(),
-               require("postcss-reporter")(),
+               require("postcss-reporter")()
                //require('pixrem')(), // add fallbacks for rem units
                //require('autoprefixer')({browsers: ["chrome > 10", "firefox > 10", "ie > 7", "android > 2", "ios > 5"]}), // add vendor prefixes
                //require('cssnano')() // minify the result
